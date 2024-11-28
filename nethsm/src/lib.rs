6e2fd916af21efefa6e95c2eabf551cfda8467ca
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-11-28 13:32:09 +0100

    feat: Add `validate_backup` to validate NetHSM backups
    This function checks the NetHSM backup for parsing errors.
    If the passphrase is provided it will do more thorough checks
    including verification of a known version number.
    
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- nethsm/src/lib.rs
+++ nethsm/src/lib.rs
@@ -88,6 +88,7 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::Display;
+use std::io::Read;
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -369,6 +370,71 @@ impl FromStr for Url {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Self::new(s)
     }
+}
+
+/// Validates a [backup].
+///
+/// Parses a previously created backup file. If `passphrase` is
+/// [`Some`], additionally decrypts the backup and verifies the
+/// encrypted backup version number.
+///
+/// # Errors
+///
+/// Returns an [`nethsm_backup::Error`] if validating a [backup] fails:
+/// * the magic number is missing in the file
+/// * the version number is unknown
+/// * the provided passphrase is incorrect
+///
+/// # Examples
+///
+/// ```no_run
+/// use nethsm::{validate_backup, ConnectionSecurity, Credentials, NetHsm, Passphrase};
+///
+/// # fn main() -> testresult::TestResult {
+/// // create a connection with a user in the Backup role
+/// let nethsm = NetHsm::new(
+///     "https://example.org/api/v1".try_into()?,
+///     ConnectionSecurity::Unsafe,
+///     Some(Credentials::new(
+///         "backup1".parse()?,
+///         Some(Passphrase::new("passphrase".to_string())),
+///     )),
+///     None,
+///     None,
+/// )?;
+///
+/// // create a backup and write it to file
+/// std::fs::write("nethsm.bkp", nethsm.backup()?)?;
+///
+/// // check for consistency only
+/// validate_backup(&mut std::fs::File::open("nethsm.bkp")?, None)?;
+///
+/// // check for correct passphrase by decrypting and validating the encrypted backup version
+/// validate_backup(
+///     &mut std::fs::File::open("nethsm.bkp")?,
+///     Passphrase::new("a sample password".into()),
+/// )?;
+/// # Ok(())
+/// # }
+/// ```
+/// [backup]: https://docs.nitrokey.com/nethsm/administration#backup
+pub fn validate_backup(
+    reader: &mut impl Read,
+    passphrase: impl Into<Option<Passphrase>>,
+) -> Result<(), nethsm_backup::Error> {
+    let passphrase = passphrase.into();
+    let backup = nethsm_backup::Backup::parse(reader)?;
+    if let Some(passphrase) = passphrase {
+        let decryptor = backup.decrypt(passphrase.expose_borrowed().as_bytes())?;
+        let version = decryptor.version()?;
+        if version.len() != 1 || version[0] != 0 {
+            return Err(nethsm_backup::Error::BadVersion {
+                highest_supported_version: 0,
+                backup_version: version,
+            });
+        }
+    }
+    Ok(())
 }
 
 /// A network connection to a NetHSM.
