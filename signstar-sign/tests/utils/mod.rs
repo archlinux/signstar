d89c02e25605be84e524129594a8110f3f248e44
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2025-10-10 11:19:49 +0200

    feat: Add support for YubiHSM signing to `signstar-sign`
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- signstar-sign/tests/utils/mod.rs
+++ signstar-sign/tests/utils/mod.rs
@@ -3,3 +3,5 @@
 pub const SIGNSTAR_CONFIG_PLAINTEXT: &[u8] =
     include_bytes!("../fixtures/signstar-config-plaintext.toml");
 pub const SIGNSTAR_CONFIG_FULL: &[u8] = include_bytes!("../fixtures/signstar-config-full.toml");
+#[cfg(feature = "yubihsm2")]
+pub const SIGNSTAR_CONFIG_YUBI: &[u8] = include_bytes!("../fixtures/signstar-config-yubi.toml");
