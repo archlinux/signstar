98cbc38f253bad74755999cf8bca134afb27ff47
David Runge <dvzrv@archlinux.org>
2025-10-20 16:41:54 +0200

    feat: Add `AsRef<UserId>` for `SystemWideUserId`
    Sometimes consumers need to rely on the inner `UserId`.
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/config/credentials.rs
+++ signstar-config/src/config/credentials.rs
@@ -201,6 +201,12 @@ impl SystemWideUserId {
     }
 }
 
+impl AsRef<UserId> for SystemWideUserId {
+    fn as_ref(&self) -> &UserId {
+        &self.0
+    }
+}
+
 impl Display for SystemWideUserId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.fmt(f)
