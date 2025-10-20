5f679a0184705c5f06d42afe1de058387ea1092f
David Runge <dvzrv@archlinux.org>
2025-10-20 17:02:12 +0200

    feat: Add `Passphrase::generate` to generate new passphrases
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-crypto/src/passphrase.rs
+++ signstar-crypto/src/passphrase.rs
@@ -2,6 +2,7 @@
 
 use std::{fmt::Display, str::FromStr};
 
+use rand::{Rng, distributions::Alphanumeric, thread_rng};
 use secrecy::{ExposeSecret, SecretString};
 use serde::{Deserialize, Serialize};
 
@@ -21,6 +22,9 @@ pub enum Error {
 pub struct Passphrase(SecretString);
 
 impl Passphrase {
+    /// The default passphrase length.
+    pub const DEFAULT_LENGTH: usize = 30;
+
     /// Creates a new [`Passphrase`] from owned [`String`]
     ///
     /// # Examples
@@ -31,6 +35,38 @@ impl Passphrase {
     /// ```
     pub fn new(passphrase: String) -> Self {
         Self(SecretString::new(passphrase.into()))
+    }
+
+    /// Generates a new [`Passphrase`].
+    ///
+    /// The generated passphrase will consist of alphanumeric characters.
+    /// The length of the passphrase can be adjusted using `length`, but is guaranteed to be at
+    /// least [`Self::DEFAULT_LENGTH`] characters long.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use signstar_crypto::passphrase::Passphrase;
+    ///
+    /// let passphrase = Passphrase::generate(None);
+    /// println!("{}", passphrase.expose_borrowed());
+    /// ```
+    pub fn generate(length: Option<usize>) -> Self {
+        let length = {
+            let mut length = length.unwrap_or(Self::DEFAULT_LENGTH);
+            if length < Self::DEFAULT_LENGTH {
+                length = Self::DEFAULT_LENGTH
+            }
+            length
+        };
+
+        Self::new(
+            thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(length)
+                .map(char::from)
+                .collect(),
+        )
     }
 
     /// Exposes the secret passphrase as owned [`String`]
@@ -75,6 +111,7 @@ impl Serialize for Passphrase {
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
     use testresult::TestResult;
 
     use super::*;
@@ -84,5 +121,14 @@ mod tests {
         let passphrase = Passphrase::new("a-secret-passphrase".to_string());
         assert_eq!(format!("{passphrase}"), "[REDACTED]");
         Ok(())
+    }
+
+    #[rstest]
+    #[case::too_short_use_default(Some(20), 30)]
+    #[case::none_use_default(None, 30)]
+    #[case::longer_than_default(Some(31), 31)]
+    fn passphrase_generate(#[case] input_length: Option<usize>, #[case] output_length: usize) {
+        let passphrase = Passphrase::generate(input_length);
+        assert_eq!(passphrase.expose_borrowed().len(), output_length);
     }
 }
