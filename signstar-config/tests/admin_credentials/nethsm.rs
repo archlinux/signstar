dc4a1d915c169b407febd6dee8840fd384d0c139
David Runge <dvzrv@archlinux.org>
2025-10-28 12:54:56 +0100

    refactor: Separate `nethsm::admin_credentials` integration tests
    Signed-off-by: David Runge <dvzrv@archlinux.org>

---
+++ signstar-config/tests/admin_credentials/nethsm.rs
@@ -0,0 +1,190 @@
+//! Integration tests for [`signstar_config::nethsm::admin_credentials`].
+use std::fs::{copy, create_dir_all, read_to_string};
+
+use nethsm::FullCredentials;
+use rstest::rstest;
+use signstar_common::admin_credentials::{
+    create_credentials_dir,
+    get_credentials_dir,
+    get_plaintext_credentials_file,
+    get_systemd_creds_credentials_file,
+};
+use signstar_config::{
+    AdminCredentials,
+    AdministrativeSecretHandling,
+    NetHsmAdminCredentials,
+    test::{get_tmp_config, write_machine_id},
+};
+use testresult::TestResult;
+
+/// Simple configuration
+const SIGNSTAR_ADMIN_CREDS_SIMPLE: &[u8] = include_bytes!("../fixtures/admin-creds-simple.toml");
+
+#[test]
+fn fail_to_load_on_path_not_a_file() -> TestResult {
+    let credentials_file = get_plaintext_credentials_file();
+    create_dir_all(&credentials_file)?;
+    let error = NetHsmAdminCredentials::load(AdministrativeSecretHandling::Plaintext)
+        .expect_err("Did not return an error!");
+    if let signstar_config::Error::AdminSecretHandling(
+        signstar_config::admin_credentials::Error::CredsFileNotAFile { path },
+    ) = error
+    {
+        assert_eq!(credentials_file, path)
+    } else {
+        panic!("Did not return an the correct error variant!")
+    }
+
+    Ok(())
+}
+
+/// Copies a plaintext TOML containing admin credentials to the default location and loads it.
+#[test]
+fn load_plaintext_toml() -> TestResult {
+    create_credentials_dir()?;
+    let config_file = get_tmp_config(SIGNSTAR_ADMIN_CREDS_SIMPLE)?;
+
+    copy(config_file, get_plaintext_credentials_file())?;
+
+    let creds = NetHsmAdminCredentials::load(AdministrativeSecretHandling::Plaintext)?;
+    println!("{creds:?}");
+    Ok(())
+}
+
+/// Stores as plaintext TOML in the default location and compares to a fixture.
+#[test]
+fn store_plaintext_toml() -> TestResult {
+    let config_file = get_tmp_config(SIGNSTAR_ADMIN_CREDS_SIMPLE)?;
+    let creds = NetHsmAdminCredentials::new(
+        1,
+        "backup-passphrase".parse()?,
+        "unlock-passphrase".parse()?,
+        vec![
+            FullCredentials::new("admin".parse()?, "admin-passphrase".parse()?),
+            FullCredentials::new("admin2".parse()?, "other-admin-passphrase".parse()?),
+        ],
+        vec![FullCredentials::new(
+            "ns1~admin".parse()?,
+            "ns1-admin-passphrase".parse()?,
+        )],
+    )?;
+    creds.store(AdministrativeSecretHandling::Plaintext)?;
+
+    let creds_string = read_to_string(get_plaintext_credentials_file())?;
+    let fixture_string = read_to_string(config_file)?;
+    assert_eq!(creds_string, fixture_string);
+    Ok(())
+}
+
+/// Stores as systemd-creds encrypted TOML in the default location and compares to a fixture.
+#[test]
+fn store_and_load_systemd_creds() -> TestResult {
+    write_machine_id()?;
+
+    let config_file = get_tmp_config(SIGNSTAR_ADMIN_CREDS_SIMPLE)?;
+
+    // load AdminCredentials from plaintext fixture
+    let creds = NetHsmAdminCredentials::load_from_file(
+        &config_file,
+        AdministrativeSecretHandling::Plaintext,
+    )?;
+
+    println!("Store systemd-creds encrypted");
+    creds.store(AdministrativeSecretHandling::SystemdCreds)?;
+
+    println!("Load systemd-creds encrypted");
+    let read_creds = NetHsmAdminCredentials::load(AdministrativeSecretHandling::SystemdCreds)?;
+
+    println!("Write systemd-creds encrypted to plaintext");
+    read_creds.store(AdministrativeSecretHandling::Plaintext)?;
+
+    println!("Compare plaintext of roundtripped and fixture");
+    let creds_string = read_to_string(get_plaintext_credentials_file())?;
+    let fixture_string = read_to_string(&config_file)?;
+    assert_eq!(creds_string, fixture_string);
+    Ok(())
+}
+
+#[rstest]
+#[case::plaintext(AdministrativeSecretHandling::Plaintext)]
+#[case::systemd_creds(AdministrativeSecretHandling::SystemdCreds)]
+fn load_admin_creds_from_default_location(
+    #[case] handling: AdministrativeSecretHandling,
+) -> TestResult {
+    write_machine_id()?;
+    create_credentials_dir()?;
+    let config_file = get_tmp_config(SIGNSTAR_ADMIN_CREDS_SIMPLE)?;
+
+    match handling {
+        AdministrativeSecretHandling::Plaintext => {
+            // make sure a dummy plaintext file exists
+            copy(config_file, get_plaintext_credentials_file())?;
+        }
+        AdministrativeSecretHandling::SystemdCreds => {
+            // load AdminCredentials from plaintext fixture and store it systemd-creds encrypted
+            let creds = NetHsmAdminCredentials::load_from_file(
+                &config_file,
+                AdministrativeSecretHandling::Plaintext,
+            )?;
+            creds.store(AdministrativeSecretHandling::SystemdCreds)?;
+        }
+        AdministrativeSecretHandling::ShamirsSecretSharing => {
+            unimplemented!("Shamir's Secret Sharing is not yet implemented!");
+        }
+    }
+    NetHsmAdminCredentials::load(handling)?;
+
+    Ok(())
+}
+
+#[rstest]
+#[case::plaintext(AdministrativeSecretHandling::Plaintext)]
+#[case::systemd_creds(AdministrativeSecretHandling::SystemdCreds)]
+fn store_admin_creds_to_default_location(
+    #[case] handling: AdministrativeSecretHandling,
+) -> TestResult {
+    write_machine_id()?;
+
+    let config_file = get_tmp_config(SIGNSTAR_ADMIN_CREDS_SIMPLE)?;
+
+    // load credentials from plaintext fixture
+    let admin_creds = NetHsmAdminCredentials::load_from_file(
+        &config_file,
+        AdministrativeSecretHandling::Plaintext,
+    )?;
+
+    // force remove any files that may be present in the persistent location
+    let _remove = std::fs::remove_dir_all(get_credentials_dir());
+    admin_creds.store(handling)?;
+
+    match handling {
+        AdministrativeSecretHandling::Plaintext => {
+            assert!(get_plaintext_credentials_file().exists());
+        }
+        AdministrativeSecretHandling::SystemdCreds => {
+            assert!(get_systemd_creds_credentials_file().exists());
+        }
+        AdministrativeSecretHandling::ShamirsSecretSharing => {
+            unimplemented!("Shamir's Secret Sharing is not yet implemented!");
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn fail_to_load_on_missing_file() -> TestResult {
+    let credentials_file = get_plaintext_credentials_file();
+    let error = NetHsmAdminCredentials::load(AdministrativeSecretHandling::Plaintext)
+        .expect_err("Did not return an error!");
+    if let signstar_config::Error::AdminSecretHandling(
+        signstar_config::admin_credentials::Error::CredsFileMissing { path },
+    ) = error
+    {
+        assert_eq!(credentials_file, path)
+    } else {
+        panic!("Did not return an the correct error variant!")
+    }
+
+    Ok(())
+}
