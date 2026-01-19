8e1eb7f1de219845bbb0bd3e04260a43ec3551d3
David Runge <dvzrv@archlinux.org>
2026-01-19 16:10:11 +0100

    feat: Implement `AsRef<Entry>` for `AuthorizedKeyEntry`
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/config/credentials.rs
+++ signstar-config/src/config/credentials.rs
@@ -121,6 +121,12 @@ impl AuthorizedKeyEntry {
     }
 }
 
+impl AsRef<Entry> for AuthorizedKeyEntry {
+    fn as_ref(&self) -> &Entry {
+        &self.0
+    }
+}
+
 impl Display for AuthorizedKeyEntry {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0.to_string())
