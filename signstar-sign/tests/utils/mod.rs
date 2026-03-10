c2aaad969647f1b447d12632f95a003ddba0d97d
David Runge <dvzrv@archlinux.org>
2026-03-10 13:04:07 +0100

    test: Imply `yubihsm2` with `mockhsm` feature and guard tests
    This switches on the `yubihsm2` feature when the `mockhsm` feature is
    used.
    In addition, the `load_credentials_for_user` test case is guarded with
    the `mockhsm` feature to ensure, that the test is excluded, when only
    the `yubihsm2` feature, but not the `mockhsm` feature is present.
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-sign/tests/utils/mod.rs
+++ signstar-sign/tests/utils/mod.rs
@@ -3,6 +3,6 @@
 pub const SIGNSTAR_CONFIG_PLAINTEXT: &[u8] =
     include_bytes!("../fixtures/signstar-config-plaintext.toml");
 pub const SIGNSTAR_CONFIG_FULL: &[u8] = include_bytes!("../fixtures/signstar-config-full.toml");
-#[cfg(feature = "yubihsm2")]
+#[cfg(feature = "mockhsm")]
 pub const SIGNSTAR_CONFIG_YUBI: &[u8] =
     include_bytes!("../fixtures/yubihsm2/signstar-config-plaintext.toml");
