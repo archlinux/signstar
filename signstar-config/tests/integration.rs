44271822e2907e750f1d915b8bb57fc9273337f7
David Runge <dvzrv@archlinux.org>
2026-03-18 16:17:11 +0100

    test: Expose common functionality for containerized integration tests
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/tests/integration.rs
+++ signstar-config/tests/integration.rs
@@ -1,4 +1,71 @@
 //! Integration tests for signstar-config modules.
+
+use std::{
+    env::var,
+    fs::copy,
+    os::unix::fs::chown,
+    path::{Path, PathBuf},
+};
+
+use log::debug;
+use signstar_config::test::list_files_in_dir;
+use testresult::TestResult;
+
+/// Environment variables that are passed in to a command call as a different user.
+const ENV_LIST: &[&str] = &[
+    "LLVM_PROFILE_FILE",
+    "CARGO_LLVM_COV",
+    "CARGO_LLVM_COV_SHOW_ENV",
+    "CARGO_LLVM_COV_TARGET_DIR",
+    "RUSTFLAGS",
+    "RUSTDOCFLAGS",
+];
+/// The location of cargo-llvm-cov `.profraw` files when running a command as a different user.
+const LLVM_PROFILE_FILE: &str = "/tmp/signstar-%p-%16m.profraw";
+
+/// Collects all `.profraw` files from `path` and copies them to `CARGO_LLVM_COV_TARGET_DIR`.
+///
+/// Only copies files from `path` if the `CARGO_LLVM_COV_TARGET_DIR` environment variable is set.
+/// Changes the ownership of files copied to `CARGO_LLVM_COV_TARGET_DIR` to root.
+///
+/// # Errors
+///
+/// Returns an error if
+///
+/// - `path` cannot be read,
+/// - an entry in `path` cannot be read,
+/// - copying a file from `path` to `CARGO_LLVM_COV_TARGET_DIR` fails,
+/// - or changing the ownership permissions of a copied file in `CARGO_LLVM_COV_TARGET_DIR` to root
+///   fails.
+fn collect_coverage_files(path: impl AsRef<Path>) -> TestResult {
+    let path = path.as_ref();
+    list_files_in_dir(path)?;
+
+    let Ok(cov_target_dir) = var("CARGO_LLVM_COV_TARGET_DIR") else {
+        return Ok(());
+    };
+    debug!("Found CARGO_LLVM_COV_TARGET_DIR={cov_target_dir}");
+    let cov_target_dir = PathBuf::from(cov_target_dir);
+
+    for dir_entry in path.read_dir()? {
+        let dir_entry = dir_entry?;
+        let from = dir_entry.path();
+        let Some(file_name) = &from.file_name() else {
+            continue;
+        };
+        if let Some(extension) = from.extension()
+            && extension == "profraw"
+        {
+            let target_file = cov_target_dir.join(file_name);
+            debug!("Copying {from:?} to {target_file:?}");
+            copy(&from, &target_file)?;
+            chown(&target_file, Some(0), Some(0))?;
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(feature = "_containerized-integration-test")]
 pub mod admin_credentials;
 
--- signstar-config/tests/non_admin_credentials/mod.rs
+++ signstar-config/tests/non_admin_credentials/mod.rs
@@ -1,15 +1,13 @@
 //! Integration tests for [`signstar_config::non_admin_credentials`].
 use std::{
     collections::HashMap,
-    env::var,
-    fs::{File, Permissions, copy, remove_file, set_permissions},
+    fs::{File, Permissions, remove_file, set_permissions},
     io::Write,
     os::unix::fs::{PermissionsExt, chown},
-    path::{Path, PathBuf},
 };
 
 use change_user_run::{CommandOutput, create_users, run_command_as_user};
-use log::{LevelFilter, debug};
+use log::LevelFilter;
 use rstest::rstest;
 use signstar_common::{
     common::{SECRET_FILE_MODE, get_data_home},
@@ -23,6 +21,8 @@ use signstar_config::{
 };
 use testresult::TestResult;
 
+use crate::{ENV_LIST, LLVM_PROFILE_FILE, collect_coverage_files};
+
 /// Plaintext configuration
 const SIGNSTAR_CONFIG_PLAINTEXT: &[u8] =
     include_bytes!("../fixtures/signstar-config-plaintext.toml");
@@ -31,61 +31,6 @@ const SIGNSTAR_CONFIG_PLAINTEXT: &[u8] =
 const SIGNSTAR_CONFIG_FULL: &[u8] = include_bytes!("../fixtures/signstar-config-full.toml");
 
 const GET_CREDENTIALS_PAYLOAD: &str = "/usr/local/bin/examples/get-nethsm-credentials";
-
-/// Environment variables that are passed in to a command call as a different user.
-const ENV_LIST: &[&str] = &[
-    "LLVM_PROFILE_FILE",
-    "CARGO_LLVM_COV",
-    "CARGO_LLVM_COV_SHOW_ENV",
-    "CARGO_LLVM_COV_TARGET_DIR",
-    "RUSTFLAGS",
-    "RUSTDOCFLAGS",
-];
-/// The location of cargo-llvm-cov `.profraw` files when running a command as a different user.
-const LLVM_PROFILE_FILE: &str = "/tmp/signstar-%p-%16m.profraw";
-
-/// Collects all `.profraw` files from `path` and copies them to `CARGO_LLVM_COV_TARGET_DIR`.
-///
-/// Only copies files from `path` if the `CARGO_LLVM_COV_TARGET_DIR` environment variable is set.
-/// Changes the ownership of files copied to `CARGO_LLVM_COV_TARGET_DIR` to root.
-///
-/// # Errors
-///
-/// Returns an error if
-///
-/// - `path` cannot be read,
-/// - an entry in `path` cannot be read,
-/// - copying a file from `path` to `CARGO_LLVM_COV_TARGET_DIR` fails,
-/// - or changing the ownership permissions of a copied file in `CARGO_LLVM_COV_TARGET_DIR` to root
-///   fails.
-fn collect_coverage_files(path: impl AsRef<Path>) -> TestResult {
-    let path = path.as_ref();
-    list_files_in_dir(path)?;
-
-    let Ok(cov_target_dir) = var("CARGO_LLVM_COV_TARGET_DIR") else {
-        return Ok(());
-    };
-    debug!("Found CARGO_LLVM_COV_TARGET_DIR={cov_target_dir}");
-    let cov_target_dir = PathBuf::from(cov_target_dir);
-
-    for dir_entry in path.read_dir()? {
-        let dir_entry = dir_entry?;
-        let from = dir_entry.path();
-        let Some(file_name) = &from.file_name() else {
-            continue;
-        };
-        if let Some(extension) = from.extension()
-            && extension == "profraw"
-        {
-            let target_file = cov_target_dir.join(file_name);
-            debug!("Copying {from:?} to {target_file:?}");
-            copy(&from, &target_file)?;
-            chown(&target_file, Some(0), Some(0))?;
-        }
-    }
-
-    Ok(())
-}
 
 /// Loading credentials for unprivileged system users succeeds.
 ///
