3242a989fd130b92e23afc8703343ec5df114a7c
David Runge <dvzrv@archlinux.org>
2026-04-02 23:31:08 +0200

    test: Add integration tests for non-admin secrets creation/loading
    Use the `config-non-admin-backend-user-secrets` example in a
    containerized test environment to
    - load configuration files specific to the given feature set
    - create secrets for non-administrative backend users (plaintext and
      systemd-creds encrypted)
    - load secrets for non-administrative backend users
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

---
+++ signstar-config/examples/config-non-admin-backend-user-secrets.rs
@@ -0,0 +1,194 @@
+//! Example for loading and creating of secrets for non-administrative backend users based on a
+//! Signstar config.
+//!
+//! Loads a Signstar [`Config`] from default system file locations.
+//! Uses the specific [`Config::user_backend_connection`] and
+//! [`UserBackendConnection::create_non_admin_backend_user_secrets`]
+//! [`UserBackendConnection::load_non_admin_backend_user_secrets`] implementations for loading and
+//! creating of secrets.
+//!
+//! # Note
+//!
+//! This example is used in integration tests and is probably not very useful on its own.
+//! Furthermore, it requires loading of Signstar configuration files from predefined system
+//! locations and calling this executable as root or a specifically configured system user
+//! (depending on the subcommand and the configuration file).
+
+use std::process::ExitCode;
+
+use clap::Parser;
+use log::LevelFilter;
+use signstar_config::SystemUserId;
+use simplelog::{ColorChoice, TermLogger, TerminalMode};
+
+/// When any of the backends is compiled in.
+#[cfg(any(feature = "nethsm", feature = "yubihsm2"))]
+mod impl_any {
+    use signstar_config::config::{
+        Config,
+        NonAdminBackendUserIdFilter,
+        NonAdminBackendUserIdKind,
+        UserBackendConnectionFilter,
+    };
+    use signstar_crypto::traits::UserWithPassphrase;
+
+    use super::*;
+
+    /// Prints the user name and passphrase of each user in a list to stdout.
+    fn print_creds_list(creds_list: Vec<Box<dyn UserWithPassphrase>>) {
+        for creds in creds_list {
+            println!(
+                "backend user: {}\npassphrase: {}",
+                creds.user(),
+                creds.passphrase().expose_borrowed()
+            );
+        }
+    }
+
+    /// Runs the command.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if
+    ///
+    /// - reading a Signstar config from a supported location fails
+    /// - creation of the secret for a non-administrative backend user fails
+    /// - loading of the secret for a non-administrative backend user fails
+    pub fn run_command(cli: Cli) -> Result<(), Error> {
+        let config = Config::from_system_path()?;
+
+        match cli.command {
+            Command::Create => {
+                let creds_list = {
+                    let user_backend_connections =
+                        config.user_backend_connections(UserBackendConnectionFilter::NonAdmin);
+
+                    let mut creds_list = Vec::new();
+                    for user_backend_connection in user_backend_connections {
+                        if let Some(creds) =
+                            user_backend_connection.create_non_admin_backend_user_secrets()?
+                        {
+                            creds_list.extend(creds);
+                        }
+                    }
+                    creds_list
+                };
+
+                print_creds_list(creds_list);
+            }
+            Command::Load(command) => {
+                if let Some(user_backend_connection) =
+                    config.user_backend_connection(&command.system_user_id)
+                {
+                    if let Some(creds_list) = user_backend_connection
+                        .load_non_admin_backend_user_secrets(NonAdminBackendUserIdFilter {
+                            backend_user_id_kind: NonAdminBackendUserIdKind::Any,
+                        })?
+                    {
+                        print_creds_list(creds_list);
+                    }
+                } else {
+                    println!(
+                        "The Unix user {} is not connected to a non-administrative backend user",
+                        &command.system_user_id
+                    );
+                };
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// When no backends are compiled in, this executable can't really do anything.
+#[cfg(all(not(feature = "nethsm"), not(feature = "yubihsm2")))]
+mod impl_none {
+    use super::*;
+
+    /// Runs the command.
+    ///
+    /// # Note
+    ///
+    /// Does nothing as there is no backend.
+    pub fn run_command(_cli: Cli) -> Result<(), Error> {
+        panic!("Without a backend, this example does not work!")
+    }
+}
+
+#[cfg(any(feature = "nethsm", feature = "yubihsm2"))]
+use impl_any::run_command;
+#[cfg(all(not(feature = "nethsm"), not(feature = "yubihsm2")))]
+use impl_none::run_command;
+
+/// An error that may occur when using config-non-admin-backend-user-secrets.
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// Expected a system user.
+    #[error("The Unix user {system_user_id} has no mapping.")]
+    NoMappingForUnixUser {
+        /// The name of the Unix user without a mapping.
+        system_user_id: SystemUserId,
+    },
+
+    /// A `signstar_config::Error`.
+    #[error(transparent)]
+    SignstarConfig(#[from] signstar_config::Error),
+}
+
+#[derive(Debug, Parser)]
+#[command(about, author, version)]
+struct LoadCommand {
+    /// The name of the Unix user for which to load non-administrative backend credentials.
+    #[arg(
+        help = "The name of the Unix user for which to load non-administrative backend credentials."
+    )]
+    pub system_user_id: SystemUserId,
+}
+
+#[derive(Debug, Parser)]
+#[command(about, author, version)]
+enum Command {
+    /// Create secrets for all non-administrative backend users.
+    #[command(about = "Create secrets for all non-administrative backend users.")]
+    Create,
+
+    /// Load a secret for a non-administrative user.
+    #[command(about = "Load a secret for a non-administrative user.")]
+    Load(LoadCommand),
+}
+
+#[derive(Debug, Parser)]
+#[command(
+    about = "Load and write non-administrative user secrets based on user mapping data",
+    version
+)]
+struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+/// Initializes a logger.
+fn init_logger() {
+    if TermLogger::init(
+        LevelFilter::Debug,
+        simplelog::Config::default(),
+        TerminalMode::Stderr,
+        ColorChoice::Auto,
+    )
+    .is_err()
+    {
+        eprintln!("Not initializing another logger, as one is initialized already.");
+    }
+}
+
+/// Runs the selected task against a created user mapping implementation.
+fn main() -> ExitCode {
+    init_logger();
+
+    if let Err(error) = run_command(Cli::parse()) {
+        eprintln!("{error}");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
+}
--- signstar-config/tests/config/mod.rs
+++ signstar-config/tests/config/mod.rs
@@ -1,6 +1,12 @@
 //! Integration tests for [`signstar_config::config`].
-use std::{fs::copy, path::PathBuf};
+use std::{collections::HashMap, io::Write, str::FromStr};
+use std::{
+    fs::{File, copy, create_dir_all},
+    path::PathBuf,
+};
 
+use change_user_run::{CommandOutput, create_users, run_command_as_user};
+use log::{LevelFilter, debug};
 use nix::unistd::{User, geteuid};
 use rstest::rstest;
 use signstar_common::config::{
@@ -17,8 +23,15 @@ use signstar_common::config::{
     get_usr_local_override_config_file_path,
     get_usr_local_override_dir_path,
 };
-use signstar_config::{SignstarConfig, SystemUserId, test::get_tmp_config};
+use signstar_common::{logging::setup_logging, system_user::get_home_base_dir_path};
+use signstar_config::{SignstarConfig, SystemUserId, config::Config, test::get_tmp_config};
+use signstar_config::{
+    config::ConfigSystemUserIds,
+    test::{start_credentials_socket, write_machine_id},
+};
 use testresult::TestResult;
+
+use crate::{ENV_LIST, LLVM_PROFILE_FILE, collect_coverage_files};
 
 /// Full configuration
 const SIGNSTAR_CONFIG_FULL: &[u8] = include_bytes!("../fixtures/signstar-config-full.toml");
@@ -105,4 +118,352 @@ fn signstar_config_store_fails_on_parent() -> TestResult {
     }
 
     Ok(())
+}
+
+/// Ensures, that [`Config::first_existing_system_path`] reports a file in the correct location.
+#[rstest]
+#[case(Config::DEFAULT_CONFIG_DIR)]
+#[case(Config::RUN_OVERRIDE_CONFIG_DIR)]
+#[case(Config::ETC_OVERRIDE_CONFIG_DIR)]
+fn config_first_existing_system_path(#[case] dir: &str) -> TestResult {
+    create_dir_all(dir)?;
+    let _file = File::create(PathBuf::from(dir).join(format!("{}.yaml", Config::CONFIG_NAME)));
+
+    assert_eq!(
+        Config::first_existing_system_path()?,
+        PathBuf::from(dir).join(format!("{}.yaml", Config::CONFIG_NAME))
+    );
+
+    Ok(())
+}
+
+/// Ensures, that [`Config::first_existing_system_path`] fails on missing configuration file.
+#[test]
+fn config_first_existing_system_path_fails_on_missing_config() -> TestResult {
+    match Config::first_existing_system_path() {
+        Ok(config) => panic!(
+            "Expected to fail with ConfigError::ConfigIsMissing but succeeded instead:\n{config:?}"
+        ),
+        Err(signstar_config::Error::Config(signstar_config::ConfigError::ConfigIsMissing)) => {}
+        Err(error) => panic!(
+            "Expected to fail with ConfigError::ConfigIsMissing but failed with a different error instead:\n{error}"
+        ),
+    }
+
+    Ok(())
+}
+
+/// The example executable to call during tests.
+const PAYLOAD: &str = "/usr/local/bin/examples/config-non-admin-backend-user-secrets";
+
+/// Tests for when using only the NetHSM backend.
+#[cfg(all(feature = "nethsm", not(feature = "yubihsm2")))]
+mod nethsm_backend {
+    use super::*;
+
+    /// Creates secrets for all configured non-administrative backend users.
+    ///
+    /// Afterwards, loads the secrets of each configured backend user by calling as the specific
+    /// configured system user associated.
+    #[rstest]
+    fn create_and_load_non_admin_secrets(
+        #[files("src/config/file/fixtures/valid_config/system-and-nethsm-config*.yaml")]
+        #[exclude("sss")]
+        #[mode = str]
+        config: &str,
+    ) -> TestResult {
+        setup_logging(LevelFilter::Debug)?;
+        write_machine_id()?;
+        let _socket = start_credentials_socket()?;
+
+        // Write config to default location
+        create_dir_all(Config::DEFAULT_CONFIG_DIR)?;
+        let mut file = File::create(Config::default_system_path())?;
+        file.write_all(config.as_bytes())?;
+
+        let config = Config::from_str(config)?;
+
+        // Create all Unix users and their homes.
+        let users = config
+            .system_user_ids()
+            .iter()
+            .cloned()
+            .map(|id| id.as_ref())
+            .collect::<Vec<_>>();
+        create_users(&users, Some(&get_home_base_dir_path()), None)?;
+
+        let CommandOutput {
+            status,
+            command,
+            stderr,
+            stdout,
+        } = run_command_as_user(
+            PAYLOAD,
+            &["create"],
+            None,
+            ENV_LIST,
+            Some(HashMap::from([(
+                "LLVM_PROFILE_FILE".to_string(),
+                LLVM_PROFILE_FILE.to_string(),
+            )])),
+            "root",
+        )?;
+        debug!("{stdout}");
+
+        if !status.success() {
+            panic!(
+                "{}",
+                signstar_config::Error::CommandNonZero {
+                    command,
+                    exit_status: status,
+                    stderr,
+                }
+            );
+        }
+
+        collect_coverage_files("/tmp")?;
+
+        for user in users {
+            let CommandOutput {
+                status,
+                command,
+                stderr,
+                stdout,
+            } = run_command_as_user(
+                PAYLOAD,
+                &["load", user],
+                None,
+                ENV_LIST,
+                Some(HashMap::from([(
+                    "LLVM_PROFILE_FILE".to_string(),
+                    LLVM_PROFILE_FILE.to_string(),
+                )])),
+                user,
+            )?;
+            debug!("{stdout}");
+
+            if !status.success() {
+                panic!(
+                    "{}",
+                    signstar_config::Error::CommandNonZero {
+                        command,
+                        exit_status: status,
+                        stderr,
+                    }
+                );
+            }
+
+            collect_coverage_files("/tmp")?;
+        }
+
+        Ok(())
+    }
+}
+
+/// Tests for when using only the YubiHSM2 backend.
+#[cfg(all(feature = "yubihsm2", not(feature = "nethsm")))]
+mod yubihsm2_backend {
+    use super::*;
+
+    /// Creates secrets for all configured non-administrative backend users.
+    ///
+    /// Afterwards, loads the secrets of each configured backend user by calling as the specific
+    /// configured system user associated.
+    #[rstest]
+    fn create_and_load_non_admin_secrets(
+        #[files("src/config/file/fixtures/valid_config/system-and-yubihsm2-config*.yaml")]
+        #[exclude("sss")]
+        #[mode = str]
+        config: &str,
+    ) -> TestResult {
+        setup_logging(LevelFilter::Debug)?;
+        write_machine_id()?;
+        let _socket = start_credentials_socket()?;
+
+        // Write config to default location
+        create_dir_all(Config::DEFAULT_CONFIG_DIR)?;
+        let mut file = File::create(Config::default_system_path())?;
+        file.write_all(config.as_bytes())?;
+
+        let config = Config::from_str(config)?;
+
+        // Create all Unix users and their homes.
+        let users = config
+            .system_user_ids()
+            .iter()
+            .cloned()
+            .map(|id| id.as_ref())
+            .collect::<Vec<_>>();
+        create_users(&users, Some(&get_home_base_dir_path()), None)?;
+
+        let CommandOutput {
+            status,
+            command,
+            stderr,
+            stdout,
+        } = run_command_as_user(
+            PAYLOAD,
+            &["create"],
+            None,
+            ENV_LIST,
+            Some(HashMap::from([(
+                "LLVM_PROFILE_FILE".to_string(),
+                LLVM_PROFILE_FILE.to_string(),
+            )])),
+            "root",
+        )?;
+        debug!("{stdout}");
+
+        if !status.success() {
+            panic!(
+                "{}",
+                signstar_config::Error::CommandNonZero {
+                    command,
+                    exit_status: status,
+                    stderr,
+                }
+            );
+        }
+
+        collect_coverage_files("/tmp")?;
+
+        for user in users {
+            let CommandOutput {
+                status,
+                command,
+                stderr,
+                stdout,
+            } = run_command_as_user(
+                PAYLOAD,
+                &["load", user],
+                None,
+                ENV_LIST,
+                Some(HashMap::from([(
+                    "LLVM_PROFILE_FILE".to_string(),
+                    LLVM_PROFILE_FILE.to_string(),
+                )])),
+                user,
+            )?;
+            debug!("{stdout}");
+
+            if !status.success() {
+                panic!(
+                    "{}",
+                    signstar_config::Error::CommandNonZero {
+                        command,
+                        exit_status: status,
+                        stderr,
+                    }
+                );
+            }
+
+            collect_coverage_files("/tmp")?;
+        }
+
+        Ok(())
+    }
+}
+
+/// Tests for when using all backends at the same time.
+#[cfg(all(feature = "nethsm", feature = "yubihsm2"))]
+mod all_backends {
+    use super::*;
+
+    /// Creates secrets for all configured non-administrative backend users.
+    ///
+    /// Afterwards, loads the secrets of each configured backend user by calling as the specific
+    /// configured system user associated.
+    #[rstest]
+    fn create_and_load_non_admin_secrets(
+        #[files("src/config/file/fixtures/valid_config/system-nethsm-and-yubihsm2-config*.yaml")]
+        #[exclude("sss")]
+        #[mode = str]
+        config: &str,
+    ) -> TestResult {
+        setup_logging(LevelFilter::Debug)?;
+        write_machine_id()?;
+        let _socket = start_credentials_socket()?;
+
+        // Write config to default location
+        create_dir_all(Config::DEFAULT_CONFIG_DIR)?;
+        let mut file = File::create(Config::default_system_path())?;
+        file.write_all(config.as_bytes())?;
+
+        let config = Config::from_str(config)?;
+
+        // Create all Unix users and their homes.
+        let users = config
+            .system_user_ids()
+            .iter()
+            .cloned()
+            .map(|id| id.as_ref())
+            .collect::<Vec<_>>();
+        create_users(&users, Some(&get_home_base_dir_path()), None)?;
+
+        let CommandOutput {
+            status,
+            command,
+            stderr,
+            stdout,
+        } = run_command_as_user(
+            PAYLOAD,
+            &["create"],
+            None,
+            ENV_LIST,
+            Some(HashMap::from([(
+                "LLVM_PROFILE_FILE".to_string(),
+                LLVM_PROFILE_FILE.to_string(),
+            )])),
+            "root",
+        )?;
+        debug!("{stdout}");
+
+        if !status.success() {
+            panic!(
+                "{}",
+                signstar_config::Error::CommandNonZero {
+                    command,
+                    exit_status: status,
+                    stderr,
+                }
+            );
+        }
+
+        collect_coverage_files("/tmp")?;
+
+        for user in users {
+            let CommandOutput {
+                status,
+                command,
+                stderr,
+                stdout,
+            } = run_command_as_user(
+                PAYLOAD,
+                &["load", user],
+                None,
+                ENV_LIST,
+                Some(HashMap::from([(
+                    "LLVM_PROFILE_FILE".to_string(),
+                    LLVM_PROFILE_FILE.to_string(),
+                )])),
+                user,
+            )?;
+            debug!("{stdout}");
+
+            if !status.success() {
+                panic!(
+                    "{}",
+                    signstar_config::Error::CommandNonZero {
+                        command,
+                        exit_status: status,
+                        stderr,
+                    }
+                );
+            }
+
+            collect_coverage_files("/tmp")?;
+        }
+
+        Ok(())
+    }
 }
