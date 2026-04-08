f7cc552ae80292e55a8ea1c6853d66a656933ecf
David Runge <dvzrv@archlinux.org>
2026-04-08 15:25:43 +0200

    test: Add helpers to `test` module for preparing systems with a `Config`
    Rely on a `SystemPrepareConfig` to centrally create preconfigured
    configuration files, system users, backend user secrets and other
    required integration.
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/test.rs
+++ signstar-config/src/test.rs
@@ -1,13 +1,17 @@
 //! Utilities used for test setups.
+
 use std::{
-    fs::{Permissions, create_dir_all, read_dir, set_permissions, write},
+    fs::{File, Permissions, create_dir_all, read_dir, set_permissions, write},
+    io::Write,
     os::{linux::fs::MetadataExt, unix::fs::PermissionsExt},
     path::{Path, PathBuf},
     process::{Child, Command},
+    str::FromStr,
     thread,
     time,
 };
 
+use change_user_run::create_users;
 use log::debug;
 #[cfg(feature = "nethsm")]
 use nethsm::{FullCredentials, UserId};
@@ -15,11 +19,13 @@ use nethsm::{FullCredentials, UserId};
 use rand::{Rng, distributions::Alphanumeric, thread_rng};
 #[cfg(feature = "nethsm")]
 use signstar_common::config::get_default_config_file_path;
+use signstar_common::system_user::get_home_base_dir_path;
 #[cfg(feature = "nethsm")]
 use signstar_crypto::passphrase::Passphrase;
 use tempfile::NamedTempFile;
 use which::which;
 
+use crate::config::{Config, ConfigSystemUserIds};
 #[cfg(feature = "nethsm")]
 use crate::{
     AdminCredentials,
@@ -28,6 +34,119 @@ use crate::{
     NetHsmAdminCredentials,
     SignstarConfig,
 };
+
+/// When any of the HSM backends is present.
+#[cfg(any(feature = "nethsm", feature = "yubihsm2"))]
+pub mod impl_any {
+    use super::*;
+    use crate::config::UserBackendConnectionFilter;
+
+    impl SystemUserConfig {
+        /// Applies the chosen system user configuration items based on a [`Config`].
+        ///
+        /// # Errors
+        ///
+        /// Returns an error if secrets for a non-administrative backend user cannot be created.
+        pub fn apply(&self, config: &Config) -> Result<(), Error> {
+            if self.create_secrets {
+                let user_backend_connections =
+                    config.user_backend_connections(UserBackendConnectionFilter::NonAdmin);
+
+                for user_backend_connection in user_backend_connections {
+                    user_backend_connection.create_non_admin_backend_user_secrets()?;
+                }
+            }
+
+            Ok(())
+        }
+    }
+}
+
+/// When no HSM backend is present.
+#[cfg(not(any(feature = "nethsm", feature = "yubihsm2")))]
+mod impl_none {
+    use super::*;
+
+    impl SystemUserConfig {
+        /// Applies the chosen system user configuration items based on a [`Config`].
+        ///
+        /// # Note
+        ///
+        /// Without any HSM backends, no backend-related actions are taken.
+        ///
+        /// # Errors
+        ///
+        /// Never returns an error.
+        pub fn apply(&self, _config: &Config) -> Result<(), Error> {
+            Ok(())
+        }
+    }
+}
+
+/// No HSM backend, plain administrative secrets handling.
+const NO_BACKEND_PLAIN_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/only-system-config-plaintext.yaml");
+
+/// No HSM backend, systemd-creds administrative secrets handling.
+const NO_BACKEND_SYSTEMD_CREDS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/only-system-config-systemd-creds.yaml");
+
+/// No HSM backend, Shamir's Secret Sharing administrative secrets handling.
+const NO_BACKEND_SSS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/only-system-config-sss.yaml");
+
+/// NetHSM backend, plain administrative secrets handling.
+const ONLY_NETHSM_PLAIN_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-and-nethsm-config-plaintext.yaml");
+
+/// NetHSM backend, systemd-creds administrative secrets handling.
+const ONLY_NETHSM_SYSTEMD_CREDS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-and-nethsm-config-systemd-creds.yaml");
+
+/// NetHSM backend, Shamir's Secret Sharing administrative secrets handling.
+const ONLY_NETHSM_SSS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-and-nethsm-config-sss.yaml");
+
+/// YubiHSM2 backend, plain administrative secrets handling.
+const ONLY_YUBIHSM2_PLAIN_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-and-yubihsm2-config-plaintext.yaml");
+
+/// YubiHSM2 backend, systemd-creds administrative secrets handling.
+const ONLY_YUBIHSM2_SYSTEMD_CREDS_ADMIN: &[u8] = include_bytes!(
+    "config/file/fixtures/valid_config/system-and-yubihsm2-config-systemd-creds.yaml"
+);
+
+/// YubiHSM2 backend, Shamir's Secret Sharing administrative secrets handling.
+const ONLY_YUBIHSM2_SSS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-and-yubihsm2-config-sss.yaml");
+
+/// YubiHSM2 backend, plain administrative secrets handling.
+const ONLY_YUBIHSM2_MOCKHSM_PLAIN_ADMIN: &[u8] = include_bytes!(
+    "config/file/fixtures/valid_config/system-and-yubihsm2-config-mockhsm-plaintext.yaml"
+);
+
+/// YubiHSM2 backend, systemd-creds administrative secrets handling.
+const ONLY_YUBIHSM2_MOCKHSM_SYSTEMD_CREDS_ADMIN: &[u8] = include_bytes!(
+    "config/file/fixtures/valid_config/system-and-yubihsm2-config-mockhsm-systemd-creds.yaml"
+);
+
+/// YubiHSM2 backend, Shamir's Secret Sharing administrative secrets handling.
+const ONLY_YUBIHSM2_MOCKHSM_SSS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-and-yubihsm2-config-sss.yaml");
+
+/// NetHSM and YubiHSM2 backends, plain administrative secrets handling.
+const ALL_BACKENDS_PLAIN_ADMIN: &[u8] = include_bytes!(
+    "config/file/fixtures/valid_config/system-nethsm-and-yubihsm2-config-plaintext.yaml"
+);
+
+/// NetHSM and YubiHSM2 backends, systemd-creds administrative secrets handling.
+const ALL_BACKENDS_SYSTEMD_CREDS_ADMIN: &[u8] = include_bytes!(
+    "config/file/fixtures/valid_config/system-nethsm-and-yubihsm2-config-systemd-creds.yaml"
+);
+
+/// NetHSM and YubiHSM2 backends, Shamir's Secret Sharing administrative secrets handling.
+const ALL_BACKENDS_SSS_ADMIN: &[u8] =
+    include_bytes!("config/file/fixtures/valid_config/system-nethsm-and-yubihsm2-config-sss.yaml");
 
 /// An error that may occur when using test utils.
 #[derive(Debug, thiserror::Error)]
@@ -41,6 +160,10 @@ pub enum Error {
         /// The source error.
         source: std::io::Error,
     },
+
+    /// A [`change_user_run::Error`] occurred.
+    #[error(transparent)]
+    ChangeUserRun(#[from] change_user_run::Error),
 
     /// A directory can not be created.
     #[error("Unable to create directory {dir}:\n{source}")]
@@ -102,6 +225,287 @@ pub enum Error {
         /// The source error.
         source: std::io::Error,
     },
+}
+
+/// The targeted location for a Signstar configuration file.
+#[derive(Clone, Copy, Debug, Default)]
+pub enum ConfigFileLocation {
+    /// The override location in `/run/signstar/`.
+    Run,
+
+    /// The override location in `/etc/signstar/`.
+    Etc,
+
+    /// The default location in `/usr/share/signstar/`.
+    #[default]
+    UsrShare,
+}
+
+impl ConfigFileLocation {
+    /// Returns the path of the configuration file's parent directory.
+    pub fn to_parent_dir_path(&self) -> PathBuf {
+        match self {
+            ConfigFileLocation::Run => PathBuf::from(Config::RUN_OVERRIDE_CONFIG_DIR),
+            ConfigFileLocation::Etc => PathBuf::from(Config::ETC_OVERRIDE_CONFIG_DIR),
+            ConfigFileLocation::UsrShare => PathBuf::from(Config::DEFAULT_CONFIG_DIR),
+        }
+    }
+}
+
+impl From<ConfigFileLocation> for PathBuf {
+    fn from(value: ConfigFileLocation) -> Self {
+        value
+            .to_parent_dir_path()
+            .join(format!("{}.yaml", Config::CONFIG_NAME))
+    }
+}
+
+/// The Signstar configuration file variant used for the file contents.
+#[derive(Clone, Copy, Debug, Default)]
+pub enum ConfigFileVariant {
+    /// No HSM backend, plain administrative secrets handling.
+    NoBackendPlainAdmin,
+
+    /// No HSM backend, systemd-creds administrative secrets handling.
+    NoBackendSystemdCredsAdmin,
+
+    /// No HSM backend, Shamir's Secret Sharing administrative secrets handling.
+    NoBackendSssAdmin,
+
+    /// NetHSM backend, plain administrative secrets handling.
+    OnlyNetHsmBackendPlainAdmin,
+
+    /// NetHSM backend, systemd-creds administrative secrets handling.
+    OnlyNetHsmBackendSystemdCredsAdmin,
+
+    /// NetHSM backend, Shamir's Secret Sharing administrative secrets handling.
+    OnlyNetHsmBackendSssAdmin,
+
+    /// YubiHSM2 backend, plain administrative secrets handling.
+    OnlyYubiHsm2BackendPlainAdmin,
+
+    /// YubiHSM2 backend, systemd-creds administrative secrets handling.
+    OnlyYubiHsm2BackendSystemdCredsAdmin,
+
+    /// YubiHSM2 backend, Shamir's Secret Sharing administrative secrets handling.
+    OnlyYubiHsm2BackendSssAdmin,
+
+    /// YubiHSM2 backend, plain administrative secrets handling.
+    OnlyYubiHsm2MockHsmBackendPlainAdmin,
+
+    /// YubiHSM2 backend, systemd-creds administrative secrets handling.
+    OnlyYubiHsm2MockHsmBackendSystemdCredsAdmin,
+
+    /// YubiHSM2 backend, Shamir's Secret Sharing administrative secrets handling.
+    OnlyYubiHsm2MockHsmBackendSssAdmin,
+
+    /// NetHSM and YubiHSM2 backends, plain administrative secrets handling.
+    AllBackendsPlainAdmin,
+
+    /// NetHSM and YubiHSM2 backends, systemd-creds administrative secrets handling.
+    AllBackendsSystemdCredsAdmin,
+
+    /// NetHSM and YubiHSM2 backends, Shamir's Secret Sharing administrative secrets handling.
+    #[default]
+    AllBackendsSssAdmin,
+}
+
+impl ConfigFileVariant {
+    /// Returns the bytes of a Signstar configuration matching the chosen variant.
+    pub fn as_config_bytes(&self) -> &[u8] {
+        match self {
+            ConfigFileVariant::NoBackendPlainAdmin => NO_BACKEND_PLAIN_ADMIN,
+            ConfigFileVariant::NoBackendSystemdCredsAdmin => NO_BACKEND_SYSTEMD_CREDS_ADMIN,
+            ConfigFileVariant::NoBackendSssAdmin => NO_BACKEND_SSS_ADMIN,
+            ConfigFileVariant::OnlyNetHsmBackendPlainAdmin => ONLY_NETHSM_PLAIN_ADMIN,
+            ConfigFileVariant::OnlyNetHsmBackendSystemdCredsAdmin => {
+                ONLY_NETHSM_SYSTEMD_CREDS_ADMIN
+            }
+            ConfigFileVariant::OnlyNetHsmBackendSssAdmin => ONLY_NETHSM_SSS_ADMIN,
+            ConfigFileVariant::OnlyYubiHsm2BackendPlainAdmin => ONLY_YUBIHSM2_PLAIN_ADMIN,
+            ConfigFileVariant::OnlyYubiHsm2BackendSystemdCredsAdmin => {
+                ONLY_YUBIHSM2_SYSTEMD_CREDS_ADMIN
+            }
+            ConfigFileVariant::OnlyYubiHsm2BackendSssAdmin => ONLY_YUBIHSM2_SSS_ADMIN,
+            ConfigFileVariant::OnlyYubiHsm2MockHsmBackendPlainAdmin => {
+                ONLY_YUBIHSM2_MOCKHSM_PLAIN_ADMIN
+            }
+            ConfigFileVariant::OnlyYubiHsm2MockHsmBackendSystemdCredsAdmin => {
+                ONLY_YUBIHSM2_MOCKHSM_SYSTEMD_CREDS_ADMIN
+            }
+            ConfigFileVariant::OnlyYubiHsm2MockHsmBackendSssAdmin => {
+                ONLY_YUBIHSM2_MOCKHSM_SSS_ADMIN
+            }
+            ConfigFileVariant::AllBackendsPlainAdmin => ALL_BACKENDS_PLAIN_ADMIN,
+            ConfigFileVariant::AllBackendsSystemdCredsAdmin => ALL_BACKENDS_SYSTEMD_CREDS_ADMIN,
+            ConfigFileVariant::AllBackendsSssAdmin => ALL_BACKENDS_SSS_ADMIN,
+        }
+    }
+
+    /// Creates a [`Config`] from the selected configuration variant in `self`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if
+    ///
+    /// - the bytes cannot be converted to a valid UTF-8 string
+    /// - a valid [`Config`] cannot be created from the variant
+    pub fn to_config(&self) -> Result<Config, crate::Error> {
+        Config::from_str(
+            &String::from_utf8(self.as_config_bytes().to_vec()).map_err(|source| {
+                crate::Error::Utf8String {
+                    path: PathBuf::from("/dev/null"),
+                    context: "creating a Signstar config object from config fixture bytes"
+                        .to_string(),
+                    source,
+                }
+            })?,
+        )
+    }
+}
+
+/// Configuration for the creation of system users.
+#[derive(Clone, Copy, Debug, Default)]
+pub struct SystemUserConfig {
+    /// Whether to create the secrets for each system user with at least one backend user.
+    #[cfg(any(feature = "nethsm", feature = "yubihsm2"))]
+    pub create_secrets: bool,
+}
+
+/// Configuration for how and where to provide a Signstar configuration file.
+#[derive(Clone, Copy, Debug, Default)]
+pub struct ConfigFileConfig {
+    /// The optional location in which the Signstar config is placed.
+    ///
+    /// If `location` is [`None`], the Signstar config is not written to a file.
+    pub location: Option<ConfigFileLocation>,
+
+    /// The variant of Signstar configuration that is added.
+    pub variant: ConfigFileVariant,
+
+    /// The optional configuration for system users.
+    ///
+    /// # Note
+    ///
+    /// When set, this implies the creation of all system users.
+    pub system_user_config: Option<SystemUserConfig>,
+}
+
+/// Creates a configuration file in a location based on [`ConfigFileLocation`] and
+/// [`ConfigFileVariant`].
+///
+/// Creates all parent directories.
+///
+/// # Errors
+///
+/// Returns an error if
+///
+/// - the creation of parent directories fails
+/// - the configuration file cannot be created
+/// - the configuration file cannot be written to
+fn create_config(location: ConfigFileLocation, variant: ConfigFileVariant) -> Result<(), Error> {
+    create_dir_all(location.to_parent_dir_path()).map_err(|source| Error::IoPath {
+        path: location.to_parent_dir_path(),
+        context: "creating the parent directory for the Signstar config",
+        source,
+    })?;
+    let path = PathBuf::from(location);
+
+    let mut file = File::create(&path).map_err(|source| crate::Error::IoPath {
+        path: path.clone(),
+        context: "creating a Signstar configuration file",
+        source,
+    })?;
+    let config_bytes = variant.as_config_bytes();
+    file.write_all(config_bytes)
+        .map_err(|source| crate::Error::IoPath {
+            path,
+            context: "writing data to a Signstar configuration file",
+            source,
+        })?;
+
+    Ok(())
+}
+
+/// Creates all Unix users and their homes based on a [`Config`].
+///
+/// # Errors
+///
+/// Returns an error if any of the Unix users cannot be created.
+fn create_unix_users_and_homes(config: &Config) -> Result<(), Error> {
+    let users = config
+        .system_user_ids()
+        .iter()
+        .cloned()
+        .map(|id| id.as_ref())
+        .collect::<Vec<_>>();
+    Ok(create_users(&users, Some(&get_home_base_dir_path()), None)?)
+}
+
+/// Configuration on how to prepare a system for a test setup.
+#[derive(Clone, Copy, Debug)]
+pub struct SystemPrepareConfig {
+    /// Whether to write an `/etc/machine-id`.
+    pub machine_id: bool,
+
+    /// Whether to start a socket for `io.systemd.Credentials`.
+    pub credentials_socket: bool,
+
+    /// How to handle the Signstar config file.
+    pub signstar_config: ConfigFileConfig,
+}
+
+impl SystemPrepareConfig {
+    /// Applies the chosen system configuration items.
+    ///
+    /// Optionally returns the [`BackgroundProcess`] tracking an `io.systemd.Credentials`
+    /// socket.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if
+    ///
+    /// - an `/etc/machine-id` file should be written, but [`write_machine_id`] fails
+    /// - an `io.systemd.Credentials` socket should be created, but [`start_credentials_socket`]
+    ///   fails
+    /// - a configuration file should be created, but writing it fails
+    /// - the creation of system users and/or their home directories fails
+    /// - the creation of backend user secrets fails
+    pub fn apply(&self) -> Result<Option<BackgroundProcess>, Error> {
+        if self.machine_id {
+            write_machine_id()?;
+        }
+
+        let background_process = if self.credentials_socket {
+            Some(start_credentials_socket()?)
+        } else {
+            None
+        };
+
+        if let Some(config_file_location) = self.signstar_config.location {
+            create_config(config_file_location, self.signstar_config.variant)?;
+
+            if let Some(system_user_config) = self.signstar_config.system_user_config {
+                let config = Config::from_str(&String::from_utf8_lossy(
+                    self.signstar_config.variant.as_config_bytes(),
+                ))?;
+                create_unix_users_and_homes(&config)?;
+                system_user_config.apply(&config)?;
+            }
+        }
+
+        Ok(background_process)
+    }
+}
+
+impl Default for SystemPrepareConfig {
+    fn default() -> Self {
+        Self {
+            machine_id: true,
+            credentials_socket: true,
+            signstar_config: ConfigFileConfig::default(),
+        }
+    }
 }
 
 /// Recursively lists files, their permissions and ownership.
