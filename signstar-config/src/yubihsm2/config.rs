35a9857b64afa4699a59c690a893e422c5e216b0
David Runge <dvzrv@archlinux.org>
2026-04-15 15:45:51 +0200

    feat: Add `YubiHsm2Config::CAP_AUDIT_LOG` tracking audit log capabilities
    Consolidates the documentation on which capabilities are in use on the
    new `CAP_AUDIT_LOG` const.
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/yubihsm2/config.rs
+++ signstar-config/src/yubihsm2/config.rs
@@ -102,7 +102,8 @@ pub enum YubiHsm2UserMapping {
     /// # Note
     ///
     /// This variant implies, that the created [authentication key object] has all relevant
-    /// [capabilities] for audit log retrieval (i.e. `get-log-entries`).
+    /// [capabilities] for audit log retrieval (see [`YubiHsm2UserMapping::CAP_AUDIT_LOG`] for
+    /// details).
     ///
     /// [authentication key object]: https://docs.yubico.com/hardware/yubihsm-2/hsm-2-user-guide/hsm2-core-concepts.html#authentication-key-object
     /// [capabilities]: https://docs.yubico.com/hardware/yubihsm-2/hsm-2-user-guide/hsm2-core-concepts.html#capability-protocol-details
@@ -310,6 +311,15 @@ impl YubiHsm2UserMapping {
         Capability::VERIFY_HMAC,
         Capability::WRAP_DATA,
     ];
+
+    /// The list of [`Capability`] items required for [`YubiHsm2UserMapping::AuditLog`].
+    ///
+    /// Each item relates to a [capability] of the YubiHSM2 device:
+    ///
+    /// - `get-log-entries`
+    ///
+    /// [capability]: https://docs.yubico.com/hardware/yubihsm-2/hsm-2-user-guide/hsm2-core-concepts.html#capability-protocol-details
+    pub const CAP_AUDIT_LOG: &[Capability] = &[Capability::GET_LOG_ENTRIES];
 
     /// Returns the optional [`Domain`] of the [`YubiHsm2UserMapping`].
     pub fn domain(&self) -> Option<&Domain> {
