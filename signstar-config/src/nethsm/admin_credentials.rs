9063b0f7e876d7ca59f8c527d5409fd090e740e0
David Runge <dvzrv@archlinux.org>
2026-05-19 13:06:04 +0200

    feat: Allow returning validated admin users from `NetHsmAdminCredentials`
    Adds the methods `NetHsmAdminCredentials::administrators_in_config` and
    `NetHsmAdminCredentials::namespace_administrators_in_config`.
    The system-wide or namespace administrators are validated against
    a `NetHsmConfig`.
    Only if the user name of the given administrator credentials are also
    present in the `NetHsmConfig`, they are returned.
    
    Related-to: https://gitlab.archlinux.org/archlinux/signstar/-/work_items/351
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/nethsm/admin_credentials.rs
+++ signstar-config/src/nethsm/admin_credentials.rs
@@ -1,11 +1,15 @@
 //! Administrative credentials for [`NetHsm`] backends.
 
+use log::warn;
 use nethsm::{FullCredentials, Passphrase};
 #[cfg(doc)]
 use nethsm::{NetHsm, UserId};
 use serde::{Deserialize, Serialize};
 
-use crate::admin_credentials::{AdminCredentials, Error};
+use crate::{
+    admin_credentials::{AdminCredentials, Error},
+    nethsm::{NetHsmConfig, NetHsmUserMapping},
+};
 
 /// Administrative credentials.
 ///
@@ -188,6 +192,138 @@ impl NetHsmAdminCredentials {
     pub fn get_namespace_administrators(&self) -> &[FullCredentials] {
         &self.namespace_administrators
     }
+
+    /// Returns the list of system-wide administrators, also present in a [`NetHsmConfig`].
+    ///
+    /// Retrieves the list of [`NetHsmUserMapping`] instances that represent system-wide
+    /// administrators from `config`.
+    /// Filters out all [`UserId`]s that cannot be matched and emits warnings for all unmatched
+    /// ones.
+    pub fn administrators_in_config(&self, config: &NetHsmConfig) -> Vec<&FullCredentials> {
+        let user_mappings = config
+            .mappings()
+            .iter()
+            .filter(|mapping| matches!(mapping, NetHsmUserMapping::Admin(..)))
+            .collect::<Vec<_>>();
+        // Only use administrative credentials that are also available in the NetHSM config.
+        {
+            let mut user_list = Vec::new();
+
+            for creds in self.get_administrators() {
+                if !user_mappings
+                    .iter()
+                    .any(|user_mapping| user_mapping.nethsm_user_ids().contains(&creds.name))
+                {
+                    warn!(
+                        "The administrative credentials for system-wide administrator {} are skipped because the user is not found in the Signstar configuration.",
+                        creds.name
+                    );
+                    continue;
+                }
+                user_list.push(creds);
+            }
+            // The available user IDs.
+            let available_users = user_list
+                .iter()
+                .map(|creds| &creds.name)
+                .collect::<Vec<_>>();
+
+            let unmatched_config_users = user_mappings
+                .iter()
+                .flat_map(|user_mapping| {
+                    user_mapping
+                        .nethsm_user_ids()
+                        .iter()
+                        .filter(|user_id| !available_users.contains(user_id))
+                        .cloned()
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            if !unmatched_config_users.is_empty() {
+                warn!(
+                    "The following system-wide administrators (R-Administrators) in the Signstar configuration are skipped, because they cannot be found in the provided administrative credentials: {}",
+                    unmatched_config_users
+                        .iter()
+                        .map(ToString::to_string)
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                );
+            }
+
+            user_list
+        }
+    }
+
+    /// Returns the list of namespace administrators, also present in a [`NetHsmConfig`].
+    ///
+    /// Retrieves the list of [`NetHsmUserMapping`] instances that represent namespace
+    /// administrators from `config`.
+    /// Filters out all [`UserId`]s that cannot be matched and emits warnings for all unmatched
+    /// ones.
+    pub fn namespace_administrators_in_config(
+        &self,
+        config: &NetHsmConfig,
+    ) -> Vec<&FullCredentials> {
+        // The list of namespace administrators.
+        let user_mappings = config
+            .mappings()
+            .iter()
+            .filter(|mapping| {
+                if let NetHsmUserMapping::Admin(user_id) = mapping {
+                    user_id.is_namespaced()
+                } else {
+                    false
+                }
+            })
+            .collect::<Vec<_>>();
+        // Only use administrative credentials that are also available in the NetHSM config.
+        {
+            let mut user_list = Vec::new();
+
+            for creds in self.get_namespace_administrators() {
+                if !user_mappings
+                    .iter()
+                    .any(|user_mapping| user_mapping.nethsm_user_ids().contains(&creds.name))
+                {
+                    warn!(
+                        "The administrative credentials for namespace administrator (N-Administrator) {} are skipped because the user is not found in the Signstar configuration.",
+                        creds.name
+                    );
+                    continue;
+                }
+                user_list.push(creds);
+            }
+            // The available user IDs.
+            let available_users = user_list
+                .iter()
+                .map(|creds| &creds.name)
+                .collect::<Vec<_>>();
+
+            let unmatched_config_users = user_mappings
+                .iter()
+                .flat_map(|user_mapping| {
+                    user_mapping
+                        .nethsm_user_ids()
+                        .iter()
+                        .filter(|user_id| !available_users.contains(user_id))
+                        .cloned()
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            if !unmatched_config_users.is_empty() {
+                warn!(
+                    "The following namespace administrators (N-Administrators) in the Signstar configuration are skipped, because they cannot be found in the provided administrative credentials: {}",
+                    unmatched_config_users
+                        .iter()
+                        .map(ToString::to_string)
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                );
+            }
+
+            user_list
+        }
+    }
 }
 
 impl AdminCredentials for NetHsmAdminCredentials {
@@ -265,6 +401,84 @@ impl AdminCredentials for NetHsmAdminCredentials {
                 },
             ));
         }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::BTreeSet, str::FromStr};
+
+    use nethsm::{Connection, UserId};
+    use rstest::{fixture, rstest};
+    use testresult::TestResult;
+
+    use super::*;
+
+    #[fixture]
+    fn nethsm_admin_credentials() -> TestResult<NetHsmAdminCredentials> {
+        Ok(NetHsmAdminCredentials::new(
+            1,
+            "backup-passphrase".parse()?,
+            "unlock-passphrase".parse()?,
+            vec![
+                FullCredentials::new("admin".parse()?, "admin-passphrase".parse()?),
+                FullCredentials::new("admin2".parse()?, "admin2-passphrase".parse()?),
+            ],
+            vec![
+                FullCredentials::new("ns1~admin".parse()?, "ns1~admin-passphrase".parse()?),
+                FullCredentials::new("ns1~admin2".parse()?, "ns1~admin2-passphrase".parse()?),
+            ],
+        )?)
+    }
+
+    #[fixture]
+    fn nethsm_config() -> TestResult<NetHsmConfig> {
+        Ok(NetHsmConfig::new(
+            BTreeSet::from_iter([Connection::new(
+                "https://nethsm1.example.org/".parse()?,
+                nethsm::ConnectionSecurity::Unsafe,
+            )]),
+            BTreeSet::from_iter([
+                NetHsmUserMapping::Admin("admin".parse()?),
+                NetHsmUserMapping::Admin("ns1~admin".parse()?),
+            ]),
+        )?)
+    }
+
+    #[rstest]
+    fn nethsm_admin_credentials_administrators_in_config(
+        nethsm_admin_credentials: TestResult<NetHsmAdminCredentials>,
+        nethsm_config: TestResult<NetHsmConfig>,
+    ) -> TestResult {
+        let nethsm_admin_credentials = nethsm_admin_credentials?;
+        let nethsm_config = nethsm_config?;
+        let users = nethsm_admin_credentials
+            .administrators_in_config(&nethsm_config)
+            .iter()
+            .map(|creds| creds.name.clone())
+            .collect::<Vec<_>>();
+
+        assert_eq!(users, vec![UserId::from_str("admin")?]);
+
+        Ok(())
+    }
+
+    #[rstest]
+    fn nethsm_admin_credentials_namespace_administrators_in_config(
+        nethsm_admin_credentials: TestResult<NetHsmAdminCredentials>,
+        nethsm_config: TestResult<NetHsmConfig>,
+    ) -> TestResult {
+        let nethsm_admin_credentials = nethsm_admin_credentials?;
+        let nethsm_config = nethsm_config?;
+        let users = nethsm_admin_credentials
+            .namespace_administrators_in_config(&nethsm_config)
+            .iter()
+            .map(|creds| creds.name.clone())
+            .collect::<Vec<_>>();
+
+        assert_eq!(users, vec![UserId::from_str("ns1~admin")?]);
 
         Ok(())
     }
