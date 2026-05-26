f1c537d919f813ee8db0dfcb4c608b36310675b6
David Runge <dvzrv@archlinux.org>
2026-05-26 17:51:25 +0200

    feat: Derive `Hash` for `Capabilities`
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-yubihsm2/src/object/capability.rs
+++ signstar-yubihsm2/src/object/capability.rs
@@ -56,7 +56,7 @@ impl From<&Capability> for yubihsm::Capability {
 }
 
 /// A set of capabilities of an object on a YubiHSM2.
-#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct Capabilities(BTreeSet<Capability>);
 
