2d6d6a908733896ec0ba290d550df8d6846184d8
David Runge <dvzrv@archlinux.org>
2026-09-02 19:35:53 +0200

    feat: Export types from the `yubihsm2` crate via the `yubihsm2` module
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/yubihsm2/mod.rs
+++ signstar-config/src/yubihsm2/mod.rs
@@ -12,3 +12,8 @@ pub use error::Error;
 // Re-export of types used from the signstar_yubihsm2 crate.
 pub use signstar_yubihsm2::object::Domain;
 pub use state::{YubiHsm2BackendState, YubiHsm2ConfigState, YubiHsm2Diff};
+
+/// Re-export of types from the yubihsm2 crate.
+pub mod yubihsm2_export {
+    pub use signstar_yubihsm2::yubihsm::{Connector, Id};
+}
