2a8a831f57f40975ad6bc08cfd062225c5873ecb
David Runge <dvzrv@archlinux.org>
2026-09-03 12:10:06 +0200

    feat!: Export types from `signstar-yubihsm2` via the `yubihsm2` module
    This changes moves the existing re-exports into a dedicated public
    module within the `yubihsm2` module.
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-config/src/yubihsm2/mod.rs
+++ signstar-config/src/yubihsm2/mod.rs
@@ -9,11 +9,14 @@ mod state;
 pub use backend::YubiHsm2Backend;
 pub use config::{Error as YubiHSM2ConfigError, YubiHsm2Config, YubiHsm2UserMapping};
 pub use error::Error;
-// Re-export of types used from the signstar_yubihsm2 crate.
-pub use signstar_yubihsm2::object::Domain;
 pub use state::{YubiHsm2BackendState, YubiHsm2ConfigState, YubiHsm2Diff};
 
 /// Re-export of types from the yubihsm2 crate.
 pub mod yubihsm2_export {
     pub use signstar_yubihsm2::yubihsm::{Connector, Id};
+}
+
+/// Re-export of types from the signstar-yubihsm2 crate.
+pub mod signstar_yubihsm2_export {
+    pub use signstar_yubihsm2::{Credentials, object::Domain};
 }
