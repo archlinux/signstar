24247016b4659176806311d04ca2abd9a97885d3
David Runge <dvzrv@archlinux.org>
2026-09-21 20:27:22 +0200

    fix: Disable all SSH forwarding features for Signstar host system users
    Related-to: https://man.archlinux.org/man/sshd_config.5#DisableForwarding
    Fixes: https://gitlab.archlinux.org/archlinux/signstar/-/work_items/400
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- signstar-configure-build/src/lib.rs
+++ signstar-configure-build/src/lib.rs
@@ -532,6 +532,7 @@ fn add_ssh_integration(
                     r#"Match user {user}
     AuthorizedKeysFile /etc/ssh/signstar-user-{user}.authorized_keys
     ForceCommand /usr/bin/{force_command}
+    DisableForwarding
 "#
                 )
                 .as_bytes(),
